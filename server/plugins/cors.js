d936eb5afc12959749c46f7de1408e3d512fe300
Ziad T. Rashid <ztr1566@gmail.com>
2026-08-16 15:42:44 +0300

    fix: stabilize CORS plugin registration

--- server/plugins/cors.js
+++ server/plugins/cors.js
@@ -1,5 +1,5 @@
 import cors from '@fastify/cors';
 
-export async function corsPlugin(app) {
-  await app.register(cors, { origin: '*' });
+export function corsPlugin(app) {
+  app.register(cors, { origin: '*' });
 }
